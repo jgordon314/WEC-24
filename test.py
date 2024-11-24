2cad1a64e5f705024b99210768985164aec30a51
Jack Gordon <jackgindahouse@icloud.com>
2024-11-24 12:32:39 -0500

    test_cracked.
    test_cracked.

--- test.py
+++ test.py
@@ -14,7 +14,9 @@ def test_cracked():
     totalTasks = 0
     totalTurns = 0
     totalTasksCompleted = 0
-    for w in range(0, 100):
+    tests = 10000
+    for w in range(0, tests):
+        if(w % (tests/10) == 0) : print(w / (tests/100))
         serverList = []
         taskList = []
         for x in range(0, random.randint(10, 100)):
@@ -35,19 +37,17 @@ def test_cracked():
         result = evaluator.eval(serverList, taskList)
         finalSimContents = result.simulation_file_contents[-1]
         totalTime += finalSimContents[1]
-        totalPower += result.output_file_contents[-1][3]
         totalTurns += finalSimContents[2]
         for x in result.output_file_contents:
             totalTasks += 1
             totalTasksCompleted += x[2]
+            totalPower += x[3]
             
     print(f"Average completion rate: {(totalTasksCompleted/totalTasks * 100)}%")
-    print(f"Average turns per completed task: {(totalTurns/totalTasksCompleted)}"%)
-    print(f"Average power per completed task: %f"%(totalPower/totalTasksCompleted))
-    print(f"Average runtime per total tasks: %f"%(totalTime/totalTasks))
-        
-
-        
+    print(f"Average turns per completed task: {(totalTurns/totalTasksCompleted)}")
+    print(f"Average power per completed task: {(totalPower/totalTasksCompleted)}")
+    print(f"Average runtime per total tasks: {(totalTime/totalTasks)}")
+                
 if __name__ == "__main__":
     for x in [test_given, test_cracked]:
         print(x.__name__ + "\n")
